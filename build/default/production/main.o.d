aae0e330fa53bfcbaf625ed2718bf72e88217dae
HossamAltokhy <hossam.abdulghany853@gmail.com>
2025-02-17 23:14:54 +0200

    Added INT0, INT1

--- build/default/production/main.o.d
+++ build/default/production/main.o.d
@@ -5,6 +5,7 @@ build/default/production/main.o.d build/default/production/main.o: main.c \
  c:\program\ files\microchip\xc8\v2.32\avr\avr\include\stdint.h \
  c:\program\ files\microchip\xc8\v2.32\avr\avr\include\util\delay_basic.h \
  c:\program\ files\microchip\xc8\v2.32\avr\avr\include\math.h \
+ c:\program\ files\microchip\xc8\v2.32\avr\avr\include\avr\interrupt.h \
  c:\program\ files\microchip\xc8\v2.32\avr\avr\include\avr\io.h \
  c:\program\ files\microchip\xc8\v2.32\avr\avr\include\avr\sfr_defs.h \
  C:/Program\ Files/Microchip/MPLABX/v5.50/packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom32a.h \
@@ -26,6 +27,8 @@ c:\program\ files\microchip\xc8\v2.32\avr\avr\include\stdint.h:
 c:\program\ files\microchip\xc8\v2.32\avr\avr\include\util\delay_basic.h:
 
 c:\program\ files\microchip\xc8\v2.32\avr\avr\include\math.h:
+
+c:\program\ files\microchip\xc8\v2.32\avr\avr\include\avr\interrupt.h:
 
 c:\program\ files\microchip\xc8\v2.32\avr\avr\include\avr\io.h:
 
--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define F_CPU 16000000UL
 
 #include <util/delay.h>
-
+#include <avr/interrupt.h>
 #include <avr/io.h>
 
 #include "DIO.h"
@@ -19,40 +19,69 @@
 #include "LCD4.h"
 #include "keypad.h"
 
+
+
+#define LOW_LEVEL    0  // 11
+#define ANY_L_CHANGE  1  // 11
+#define FALLING_EDGE   2 // 11
+#define RISING_EDGE  3  // 11
+
+ISR(INT0_vect){
+    
+    
+    LED_TOG(ALL_LEDs);
+    
+}
+
+
+void init_INT0(char INT_MODE);
+void init_INT1(char INT_MODE);
+
 int main(void) {
     /* Replace with your application code */
     // initializations
-    init_LEDs();
-    init_LCD4();
-    
-    
-    init_Keypad();
-    while (1) {
 
-//        LCD4_clear();
-        LCD4_num(getKey());
-        _delay_ms(200);
-        
-//        int x = getKey();
-//        
-//        if(x == 3){
-//            LED_OFF(ALL_LEDs);
-//            LED_ON(LED1);
-//        }
-//        else if(x==6){
-//            LED_OFF(ALL_LEDs);
-//            LED_ON(LED2);
-//        }
-//        else if(x==9){
-//            LED_OFF(ALL_LEDs);
-//            LED_ON(LED3);
-//        }
-//        else{
-//            LED_OFF(ALL_LEDs);
-//            LED_ON(LED4);
-//        }
+    init_LEDs();
+    
+    init_INT0();
+    
+    // Enable Global Interrupt bit (SEI)
+    sei();
+    while (1) {
 
 
     }
+}
+
+
+void init_INT0(char INT_MODE){
+    
+    // Select MODE
+    MCUCR &= ~((1<<ISC01)|(1<<ISC00));
+    MCUCR |= INT_MODE;
+//    switch(INT_MODE){
+//        case RISING_EDGE:
+//            
+//            break;
+//    }
+    // Enable Individual INT0
+    GICR |= (1<<INT0);
+    
+    
+}
+void init_INT1(char INT_MODE){
+    
+    // Select MODE
+    MCUCR &= ~((1<<ISC11)|(1<<ISC10));
+    MCUCR |= (INT_MODE<<2);
+//    switch(INT_MODE){
+//        case RISING_EDGE:
+//            
+//            break;
+//    }
+    // Enable Individual INT0
+    GICR |= (1<<INT1);
+    
+    
 }
 
